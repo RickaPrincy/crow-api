b95fc250d56db260c06c87c92bdbef2bc778e8bc
RickaPrincy <rckprincy@gmail.com>
2023-08-22 22:38:56 +0300

    feat: add more routes

--- main.cpp
+++ main.cpp
@@ -2,10 +2,25 @@
 
 int main(){
     crow::SimpleApp app;
-    
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
-    }); 
+
+    CROW_ROUTE(app, "/")([](){ 
+        return "Hello world"; 
+    });
+
+    CROW_ROUTE(app, "/welcome")([](){
+        auto page = crow::mustache::load_text("crow-api.html");
+        return page;
+    });
+
+    CROW_ROUTE(app, "/me/<string>")([](std::string name){ 
+        auto page = crow::mustache::load("welcome-you.html"); 
+        crow::mustache::context ctx({{"name", name}});    
+        return page.render(ctx);                            
+    });
+
+    CROW_ROUTE(app, "/you").methods(crow::HTTPMethod::POST)([](){ 
+        return "Hello world from post";
+    });
 
     app.port(8080).multithreaded().run(); 
 }
